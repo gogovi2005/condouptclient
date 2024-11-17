571c11463caae03f23497b41d9251fd015c2b26c
gogovi2005 <100237881+gogovi2005@users.noreply.github.com>
2024-11-17 15:32:41 +0000

    Adição do Client
    Primeira versão do client.

---
+++ client_condoupt/src/main/java/upt/proj/condominio/client/ClientApplication.java
@@ -0,0 +1,9 @@
+package upt.proj.condominio.client;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import upt.proj.condominio.model.Conta;
+
+public class ClientApplication {
+
+}
