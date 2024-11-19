1e7d731ee5b8892fd810fc3165a7a52afe00cb1c
gogovi2005 <gogovi2005@gmail.com>
2024-11-19 17:56:39 +0000

    Remoção dos total (não deviam estar)

--- src/main/java/upt/proj/condominio/client/service/PredioClientService.java
+++ src/main/java/upt/proj/condominio/client/service/PredioClientService.java
@@ -22,16 +22,13 @@ public class PredioClientService {
         String url = BASE_URL + "/predio/" + nome;
         return restTemplate.getForObject(url, Predio.class); 
     }
-    public void createPredio(Integer donoPredId, String nomeP, String zona, Integer ntotalapart, Boolean animaisP, Float totalAgua, Float totalLuz, Float totalGas) {
+    public void createPredio(Integer donoPredId, String nomeP, String zona, Integer ntotalapart, Boolean animaisP) {
         String url = BASE_URL;
         Map<String,Object> map = new HashMap<>();
         map.put("noneP", nomeP);
         map.put("zona",zona);
         map.put("ntotalapart",ntotalapart);
         map.put("animaisP",animaisP);
-        map.put("totalAgua",totalAgua);
-        map.put("totalLuz",totalLuz);
-        map.put("totalGas",totalGas);
         map.put("donoPredId",donoPredId);
         
 
