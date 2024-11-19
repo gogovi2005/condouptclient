05014452a79d99288a1522a8ea694721631bd930
gogovi2005 <gogovi2005@gmail.com>
2024-11-19 17:53:41 +0000

    PredioClientService - Feito

--- src/main/java/upt/proj/condominio/client/service/PredioClientService.java
+++ src/main/java/upt/proj/condominio/client/service/PredioClientService.java
@@ -1,5 +1,8 @@
 package upt.proj.condominio.client.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,4 +22,22 @@ public class PredioClientService {
         String url = BASE_URL + "/predio/" + nome;
         return restTemplate.getForObject(url, Predio.class); 
     }
+    public void createPredio(Integer donoPredId, String nomeP, String zona, Integer ntotalapart, Boolean animaisP, Float totalAgua, Float totalLuz, Float totalGas) {
+        String url = BASE_URL;
+        Map<String,Object> map = new HashMap<>();
+        map.put("noneP", nomeP);
+        map.put("zona",zona);
+        map.put("ntotalapart",ntotalapart);
+        map.put("animaisP",animaisP);
+        map.put("totalAgua",totalAgua);
+        map.put("totalLuz",totalLuz);
+        map.put("totalGas",totalGas);
+        map.put("donoPredId",donoPredId);
+        
+
+        try {
+            restTemplate.postForObject(url,map,Void.class);
+        } catch (Exception e) {
+            e.printStackTrace(); }
+    	}
 }
