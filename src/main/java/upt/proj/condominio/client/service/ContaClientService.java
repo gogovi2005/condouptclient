f6e689374ad1936af783ed1efdbe1ce729d5d029
gogovi2005 <gogovi2005@gmail.com>
2024-11-19 17:55:04 +0000

    float --> Float (erro meu malta)

--- src/main/java/upt/proj/condominio/client/service/ContaClientService.java
+++ src/main/java/upt/proj/condominio/client/service/ContaClientService.java
@@ -58,7 +58,7 @@ public class ContaClientService {
             e.printStackTrace();}
     }
 
-    public void createEmpresa(String username,String email,String password,String tiposervice, String zona, float preco) {
+    public void createEmpresa(String username,String email,String password,String tiposervice, String zona, Float preco) {
         String url = BASE_URL;
         Map<String,Object> map = new HashMap<>();
         map.put("tipo","Empresa");
@@ -73,6 +73,5 @@ public class ContaClientService {
             restTemplate.postForObject(url,map,Void.class);
         } catch (Exception e) {
             e.printStackTrace(); }
-        }
-    
+        } 
     }
